be82229a9143480008b0491bdc0eb6e4aa9b811c
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-07-12 07:58:26 -0700

    Created albums seeder file

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,8 +4,8 @@ CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(50) DEFAULT 'NONE',
                         album_name  VARCHAR(100) NOT NULL,
-                        release_date DATE,
+                        release_date INT,
                         sales INT NOT NULL,
-                        genre VARCHAR(20) NOT NULL,
+                        genre VARCHAR(100) NOT NULL,
                         PRIMARY KEY (id)
 );
